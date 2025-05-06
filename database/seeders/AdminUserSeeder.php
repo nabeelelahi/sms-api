4fee3cf1b605b24e50955a0bf9ad30f91c8fdbb8
nabeelelahi <nabeelelahi2000@gmail.com>
2025-05-05 19:18:59 -0700

    feat(seeder): admin seeder added

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@admin.com'],
+            [
+                'name' => 'School Admin',
+                'password' => Hash::make('Admin@123$'),
+                'role' => User::ROLE_ADMIN,
+            ]
+        );
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,11 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // Call your custom admin seeder
+        $this->call([
+            AdminUserSeeder::class,
+        ]);
+
     }
 }

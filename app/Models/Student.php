792b87f2266fab22fb9f70ce333280c94dc03a0a
nabeelelahi <nabeelelahi2000@gmail.com>
2025-05-05 19:00:07 -0700

    feat(init): base laravel set up

---
+++ app/Models/Student.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'age',
+        'residence_location',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
